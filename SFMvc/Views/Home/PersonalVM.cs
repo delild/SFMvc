18ffac44774504cc3252ec328fadc4c46538e81e
Emma Eles Bohlin <emma.eles@brightstraining.com>
2023-09-04 13:06:14 +0200

    tagit bort databasmodellen från PersonalVM

--- SFMvc/Views/Home/PersonalVM.cs
+++ SFMvc/Views/Home/PersonalVM.cs
@@ -4,6 +4,15 @@ namespace SFMvc.Views.Home
 {
     public class PersonalVM
     {
-        public List<Show> MyWatchList { get; set; }
-    }
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Format { get; set; }
+		public string ImageUrl { get; set; }
+		public string LogoUrl { get; set; }
+		public string? StreamingUrl { get; set; }
+		public int? LengthInMinutes { get; set; }
+		public int? NumberOfEpisodes { get; set; }
+		public int? NumberOfSeasons { get; set; }
+	}
 }
