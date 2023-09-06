f36f72d739ee1d5ec570e6b489b1a53cdb89a5f5
Emma Eles Bohlin <emma.eles@brightstraining.com>
2023-09-06 16:56:46 +0200

    Made search bar glow

---
+++ SFMvc/Migrations/20230906143136_Changed crunchyroll logo.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SFMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changedcrunchyrolllogo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "LogoUrl",
+                value: "Crunchyroll_logo_2.png");
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "LogoUrl",
+                value: "Crunchyroll_logo_2.png");
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "LogoUrl",
+                value: "Crunchyroll_logo_2.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "LogoUrl",
+                value: "crunchyroll-logo.png");
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "LogoUrl",
+                value: "crunchyroll-logo.png");
+
+            migrationBuilder.UpdateData(
+                table: "Shows",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "LogoUrl",
+                value: "crunchyroll-logo.png");
+        }
+    }
+}
